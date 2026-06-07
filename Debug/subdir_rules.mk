ffa1603f57d5f3a27a10feb4f6d8f98fac7b44f8
Swapnil Waghmare <your-email@example.com>
2026-06-07 17:41:05 +0530

    Added timer driver

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -8,14 +8,14 @@ SHELL = cmd.exe
 %.o: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"D:/Swapnil_Software/Code_Composer_Studio_TI/ccs/tools/compiler/ti-cgt-armllvm_3.2.2.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Driver_Practice/TI_Driver_Developemnt" -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Driver_Practice/TI_Driver_Developemnt/Debug" -I"C:/ti/mspm0_sdk_2_10_00_04/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_2_10_00_04/source" -gdwarf-3 -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	"D:/Swapnil_Software/Code_Composer_Studio_TI/ccs/tools/compiler/ti-cgt-armllvm_3.2.2.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Timer_Driver_Prictice/TI_Driver_Development" -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Timer_Driver_Prictice/TI_Driver_Development/Debug" -I"C:/ti/mspm0_sdk_2_10_00_04/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_2_10_00_04/source" -gdwarf-3 -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
 build-81021297: ../empty.syscfg
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: SysConfig'
-	"C:/ti/sysconfig_1.26.2/sysconfig_cli.bat" --script "C:/Users/sacf n/Desktop/Git_Practice/TI_Driver_Practice/TI_Driver_Developemnt/empty.syscfg" -o "." -s "C:/ti/mspm0_sdk_2_10_00_04/.metadata/product.json" --compiler ticlang
+	"C:/ti/sysconfig_1.26.2/sysconfig_cli.bat" --script "C:/Users/sacf n/Desktop/Git_Practice/TI_Timer_Driver_Prictice/TI_Driver_Development/empty.syscfg" -o "." -s "C:/ti/mspm0_sdk_2_10_00_04/.metadata/product.json" --compiler ticlang
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
@@ -28,14 +28,14 @@ ti_msp_dl_config.h: build-81021297
 %.o: ./%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"D:/Swapnil_Software/Code_Composer_Studio_TI/ccs/tools/compiler/ti-cgt-armllvm_3.2.2.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Driver_Practice/TI_Driver_Developemnt" -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Driver_Practice/TI_Driver_Developemnt/Debug" -I"C:/ti/mspm0_sdk_2_10_00_04/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_2_10_00_04/source" -gdwarf-3 -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	"D:/Swapnil_Software/Code_Composer_Studio_TI/ccs/tools/compiler/ti-cgt-armllvm_3.2.2.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Timer_Driver_Prictice/TI_Driver_Development" -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Timer_Driver_Prictice/TI_Driver_Development/Debug" -I"C:/ti/mspm0_sdk_2_10_00_04/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_2_10_00_04/source" -gdwarf-3 -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
 startup_mspm0g350x_ticlang.o: C:/ti/mspm0_sdk_2_10_00_04/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g350x_ticlang.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"D:/Swapnil_Software/Code_Composer_Studio_TI/ccs/tools/compiler/ti-cgt-armllvm_3.2.2.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Driver_Practice/TI_Driver_Developemnt" -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Driver_Practice/TI_Driver_Developemnt/Debug" -I"C:/ti/mspm0_sdk_2_10_00_04/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_2_10_00_04/source" -gdwarf-3 -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	"D:/Swapnil_Software/Code_Composer_Studio_TI/ccs/tools/compiler/ti-cgt-armllvm_3.2.2.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Timer_Driver_Prictice/TI_Driver_Development" -I"C:/Users/sacf n/Desktop/Git_Practice/TI_Timer_Driver_Prictice/TI_Driver_Development/Debug" -I"C:/ti/mspm0_sdk_2_10_00_04/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_2_10_00_04/source" -gdwarf-3 -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
--- Main.c
+++ Main.c
@@ -37,5 +37,6 @@ int main(void)
     SYSCFG_DL_init();
 
     while (1) {
+        //xyz
     }
 }
